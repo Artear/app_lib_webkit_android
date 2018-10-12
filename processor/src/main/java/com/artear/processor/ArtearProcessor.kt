e348add1e4f5d6c18cd2efc1af3d0a016bf9c8af
daver1419 <daver1419@gmail.com>
2018-10-12 18:31:06 -0300

    Change: Set new annotation Processor whit kotlin poet ArtearProcessor

--- processor/src/main/java/com/artear/processor/ArtearProcessor.kt
+++ processor/src/main/java/com/artear/processor/ArtearProcessor.kt
@@ -1,7 +1,8 @@
 package com.artear.processor
 
 import com.artear.annotations.JsInterface
-import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.*
+import java.io.File
 import java.io.IOException
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
@@ -10,7 +11,6 @@ import javax.lang.model.element.TypeElement
 import javax.lang.model.element.VariableElement
 import javax.lang.model.util.Elements
 import javax.lang.model.util.Types
-import javax.tools.Diagnostic
 import javax.tools.Diagnostic.Kind.ERROR
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
@@ -41,22 +41,58 @@ class ArtearProcessor : AbstractProcessor() {
     }
 
     override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
-        val typeElements = roundEnv.getElementsAnnotatedWith(JsInterface::class.java).asSequence()
-                .filterIsInstance<TypeElement>()
-                .filter { isValidClass(it) }
-                .map { buildAnnotatedClass(it) }
-                .toList()
 
-        val generatedSourcesRoot: String = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME].orEmpty()
-        if (generatedSourcesRoot.isEmpty()) {
-            processingEnv.messager.printMessage(Diagnostic.Kind.ERROR,
-                    "Can't find the target directory for generated Kotlin files.")
+        val kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME] ?: run {
+            processingEnv.messager.printMessage(ERROR, "Can't find the target directory for generated Kotlin files.")
             return false
         }
 
-        generateJSInterfaceClass(typeElements)
+//        val fileSpec = FileSpec.get("test.generate.artear", generatedClass)
+
+        val greeterClass = ClassName("test.generate.artear", "Greeter")
+        val file = FileSpec.builder("test.generate.artear", "HelloWorld")
+                .addType(TypeSpec.classBuilder("Greeter")
+                        .primaryConstructor(FunSpec.constructorBuilder()
+                                .addParameter("name", String::class)
+                                .build())
+                        .addProperty(PropertySpec.builder("name", String::class)
+                                .initializer("name")
+                                .build())
+                        .addFunction(FunSpec.builder("greet")
+                                .addStatement("println(%S)", "Hello, \$name")
+                                .build())
+                        .build())
+                .addFunction(FunSpec.builder("main")
+                        .addParameter("args", String::class, KModifier.VARARG)
+                        .addStatement("%T(args[0]).greet()", greeterClass)
+                        .build())
+                .build()
+
+        file.writeTo(File(kaptKotlinGeneratedDir))
+
+
         return true
     }
+
+
+//    override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean {
+//
+//        val typeElements = roundEnv.getElementsAnnotatedWith(JsInterface::class.java).asSequence()
+//                .filterIsInstance<TypeElement>()
+//                .filter { isValidClass(it) }
+//                .map { buildAnnotatedClass(it) }
+//                .toList()
+//
+//        val generatedSourcesRoot: String = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME].orEmpty()
+//        if (generatedSourcesRoot.isEmpty()) {
+//            processingEnv.messager.printMessage(Diagnostic.Kind.ERROR,
+//                    "Can't find the target directory for generated Kotlin files.")
+//            return false
+//        }
+//
+//        generateJSInterfaceClass(typeElements)
+//        return true
+//    }
 
 
     private fun isValidClass(typeElement: TypeElement): Boolean {
@@ -67,7 +103,7 @@ class ArtearProcessor : AbstractProcessor() {
         return true
     }
 
-    private fun buildAnnotatedClass(typeElement: TypeElement) : JSInterfaceClass {
+    private fun buildAnnotatedClass(typeElement: TypeElement): JSInterfaceClass {
 
         val variableNames = typeElement.enclosedElements.asSequence()
                 .filterIsInstance<VariableElement>()
@@ -84,8 +120,7 @@ class ArtearProcessor : AbstractProcessor() {
         }
 
         for (easyJSONClass in jsInterfacesClasses) {
-            val packageName = Utils.getPackageName(processingEnv.elementUtils,
-                    easyJSONClass.typeElement)
+            val packageName = Utils.getPackageName(elements, easyJSONClass.typeElement)
             val generatedClass = CodeGenerator.generateClass(easyJSONClass)
 
             val fileSpec = FileSpec.get(packageName, generatedClass)
